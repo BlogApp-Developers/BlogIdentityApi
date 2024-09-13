b84cbcf932bcdff88af99acbf11a819a54ab8e77
Emil B <emkaran_500@mail.ru>
2024-09-13 23:24:16 +0400

    Add Follow Counts

--- src/Follow/Repositories/Base/IFollowRepository.cs
+++ src/Follow/Repositories/Base/IFollowRepository.cs
@@ -3,4 +3,4 @@ namespace BlogIdentityApi.Follow.Repositories.Base;
 using BlogIdentityApi.Base.Methods;
 using BlogIdentityApi.Follow.Models;
 
-    public interface IFollowRepository : ICreateAsync<Follow>, IDeleteAsync<Follow>, IGetsByIdAsync<Follow>, IGetByIdAsync<Follow> {}
+    public interface IFollowRepository : ICreateAsync<Follow>, IDeleteAsync<Follow>, IGetsByIdAsync<Follow>, IGetByIdAsync<Follow>, IGetsInvertByIdAsync<Follow> {}
